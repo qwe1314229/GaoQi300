5c9fd45b29e3144c711f3f301f43de716d240991
qwe1314229 <chenjiancxy@163.com>
2018-09-10 23:48:44 +0800

    day2

---
+++ src/com/day02/TestIdentifer.java
@@ -0,0 +1,11 @@
+package com.day02;
+
+public class TestIdentifer {
+	public static void main(String[] args) {
+		int abc = 0;
+		int $a = 1;
+		int _abc = 2;
+		int 汉字 = 3;
+		System.out.println(汉字);
+	}
+}
